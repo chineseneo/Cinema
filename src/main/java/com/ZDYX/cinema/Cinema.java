d1ca0a26ffa884993d7f5c0ea6953d336ba5d5b8
Longkai Cao <lkcao@thoughtworks.com>
2013-12-07 14:17:44 +0800

    Longkai: remove magic number

--- src/main/java/com/ZDYX/cinema/Cinema.java
+++ src/main/java/com/ZDYX/cinema/Cinema.java
@@ -1,27 +1,33 @@
 package com.ZDYX.cinema;
 
 public class Cinema {
+    private static final double UNIT_PRICE_2D = 30D;
+    public static final double UNIT_PRICE_3D = 60D;
+    public static final double DISCOUNT_FOR_CARD = 0.8;
+    public static final String MOVIE_TYPE_2D = "2D";
+    public static final String PAYMENT_TYPE_CASH = "cash";
+
     public Double getPriceFor(int numberOfTicket) {
-        return 30D * numberOfTicket;
+        return UNIT_PRICE_2D * numberOfTicket;
     }
 
     public Double getPriceFor(int numberOfTicket, String movieType) {
-        if ("2D".equals(movieType)) {
-            return 30D * numberOfTicket;
+        if (MOVIE_TYPE_2D.equals(movieType)) {
+            return UNIT_PRICE_2D * numberOfTicket;
         }
-        return 60D * numberOfTicket;
+        return UNIT_PRICE_3D * numberOfTicket;
     }
 
     public Double getPriceFor(int numberOfTicket, String movieType, String card) {
-        if ("cash".equals(card)) {
-            if ("2D".equals(movieType)) {
-                return 30D * numberOfTicket;
+        if (PAYMENT_TYPE_CASH.equals(card)) {
+            if (MOVIE_TYPE_2D.equals(movieType)) {
+                return UNIT_PRICE_2D * numberOfTicket;
             }
-            return 60D * numberOfTicket;
+            return UNIT_PRICE_3D * numberOfTicket;
         }
-        if ("2D".equals(movieType)) {
-            return 24D * numberOfTicket;
+        if (MOVIE_TYPE_2D.equals(movieType)) {
+            return UNIT_PRICE_2D * DISCOUNT_FOR_CARD * numberOfTicket;
         }
-        return 48D * numberOfTicket;
+        return UNIT_PRICE_3D * DISCOUNT_FOR_CARD * numberOfTicket;
     }
 }
